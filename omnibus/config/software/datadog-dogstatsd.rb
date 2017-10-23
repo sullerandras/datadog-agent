bc5782b3ac120cf762fefa62cb90fe3dc6c4eb3b
Massimiliano Pippi <mpippi@gmail.com>
2017-10-23 20:57:21 +0200

    revamp dsd omnibus installer

--- omnibus/config/software/datadog-dogstatsd.rb
+++ omnibus/config/software/datadog-dogstatsd.rb
@@ -3,18 +3,11 @@
 # This product includes software developed at Datadog (https:#www.datadoghq.com/).
 # Copyright 2017 Datadog, Inc.
 
-require './lib/ostools.rb'
-require 'pathname'
-
-name 'datadog-agent'
-
-dependency 'python'
-unless windows?
-  dependency 'net-snmp-lib'
-end
+name 'datadog-dogstatsd'
 
 license "Apache-2.0"
-license_file "../LICENSE"
+license_file "LICENSE"
+skip_transitive_dependency_licensing true
 
 source path: '..'
 relative_path 'src/github.com/DataDog/datadog-agent'
@@ -28,42 +21,32 @@ build do
   }
 
   # we assume the go deps are already installed before running omnibus
-  command "invoke agent.build --rebuild --use-embedded-libs --no-development", env: env
+  command "invoke dogstatsd.build --rebuild --use-embedded-libs", env: env
 
-  mkdir "#{install_dir}/etc/datadog-agent"
+  mkdir "#{install_dir}/etc/datadog-dogstatsd"
   unless windows?
     mkdir "#{install_dir}/run/"
     mkdir "#{install_dir}/scripts/"
   end
 
-  # if windows?
-  #   mkdir "../../extra_package_files/EXAMPLECONFSLOCATION"
-  #   copy "pkg/collector/dist/conf.d/*", "../../extra_package_files/EXAMPLECONFSLOCATION"
-  # end
-
   # move around bin and config files
-  copy 'bin', install_dir
-  move 'bin/agent/dist/datadog.yaml', "#{install_dir}/etc/datadog-agent/datadog.yaml.example"
-
-  move 'bin/agent/dist/trace-agent.conf', "#{install_dir}/etc/datadog-agent/"
-  move 'bin/agent/dist/process-agent.conf', "#{install_dir}/etc/datadog-agent/"
-
-  if windows?
-    move 'bin/agent/dist/conf.d', "#{install_dir}/etc/datadog-agent/"
-  end
+  copy 'bin/dogstatsd/dogstatsd', "#{install_dir}/bin"
+  move 'bin/dogstatsd/dist/dogstatsd.yaml', "#{install_dir}/etc/datadog-dogstatsd/dogstatsd.yaml.example"
 
   if linux?
     erb source: "upstart.conf.erb",
-        dest: "#{install_dir}/scripts/datadog-agent.conf",
+        dest: "#{install_dir}/scripts/datadog-dogstatsd.conf",
         mode: 0755,
         vars: { install_dir: install_dir }
 
     erb source: "systemd.service.erb",
-        dest: "#{install_dir}/scripts/datadog-agent.service",
+        dest: "#{install_dir}/scripts/datadog-dogstatsd.service",
         mode: 0755,
         vars: { install_dir: install_dir }
   end
 
-
+  # The file below is touched by software builds that don't put anything in the installation
+  # directory (libgcc right now) so that the git_cache gets updated let's remove it from the
+  # final package
   delete "#{install_dir}/uselessfile"
 end
